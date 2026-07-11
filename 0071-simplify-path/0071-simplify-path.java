7d6bffb914d46270e4ce3e4eb7335a40fdede83d
Ritesh Suresh Kale <rkale510544@gmail.com>
2026-07-11 22:48:02 +0530

    Time: 4 ms (96.93%), Space: 45.1 MB (38.86%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> st = new Stack<>();
+        String[] parts = path.split("/");
+
+        for(String part : parts){
+            if(part.equals("") || part.equals(".")){
+                continue;
+            }
+            else if(part.equals("..")){
+                if(!st.isEmpty()){
+                    st.pop();
+                }
+            }
+            else {
+                st.push(part);
+            }
+        }
+        StringBuilder ans = new StringBuilder();
+
+        for(String dir : st){
+            ans.append("/").append(dir);
+        }
+        if(ans.length() == 0){
+            return "/";
+        }
+        return ans.toString();
+    }
+}
