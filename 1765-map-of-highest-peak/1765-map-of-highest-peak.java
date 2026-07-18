400653ad44120161a71cf5ed0a1f8750c968ae9c
Ritesh Suresh Kale <rkale510544@gmail.com>
2026-07-18 10:51:33 +0530

    Time: 49 ms (27.14%), Space: 185.3 MB (21.24%) - LeetHub

---
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[][] highestPeak(int[][] isWater) {
+        int rows = isWater.length;
+        int cols = isWater[0].length;
+
+        int[][] distance = new int[rows][cols];
+        boolean[][] visited = new boolean[rows][cols];
+
+        Queue<int[]> q = new LinkedList<>();
+
+        for(int i =0; i<rows; i++){
+            for(int j=0; j< cols; j++){
+                if(isWater[i][j] == 1){
+                    q.offer(new int[]{i,j});
+                    visited[i][j] = true;
+                }
+            }
+        }
+        int[] dr = {-1,1,0,0};
+        int[] dc = {0,0,-1,1};
+
+        while(!q.isEmpty()){
+            int[] cell = q.poll();
+
+            int currRow = cell[0];
+            int currCol = cell[1];
+
+            for(int k =0; k< 4; k++){
+                int newRow = currRow +dr[k];
+                int newCol = currCol +dc[k];
+
+                if(newRow >=0 && newRow < rows && newCol >=0 && newCol < cols && !visited[newRow][newCol]){
+                    visited[newRow][newCol] = true;
+
+                    distance[newRow][newCol] = distance[currRow][currCol] +1;
+
+                    q.offer(new int[]{newRow,newCol});
+                }
+            }
+        }
+        return distance;
+    }
+}
