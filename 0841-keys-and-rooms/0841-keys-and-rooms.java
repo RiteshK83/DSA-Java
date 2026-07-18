5499e22dbbbe5ae62eac1af673b1647ccd9f9dfd
Ritesh Suresh Kale <rkale510544@gmail.com>
2026-07-18 11:20:28 +0530

    Time: 1 ms (84.49%), Space: 46.2 MB (64.99%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,27 +1,49 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-      Queue<Integer> q = new LinkedList<>();
-      boolean[] visited = new boolean[rooms.size()];
 
-      q.offer(0);
-      visited[0] = true;
+        // BFS Method
+//       Queue<Integer> q = new LinkedList<>();
+//       boolean[] visited = new boolean[rooms.size()];
 
-      while(!q.isEmpty()){
-        int room = q.poll(); 
+//       q.offer(0);
+//       visited[0] = true;
 
-        for(int key : rooms.get(room)){
-            if(!visited[key]){
-                visited[key] = true;
-                q.offer(key);
-            }
-        }
-      }
-      for(boolean room : visited){
+//       while(!q.isEmpty()){
+//         int room = q.poll(); 
+
+//         for(int key : rooms.get(room)){
+//             if(!visited[key]){
+//                 visited[key] = true;
+//                 q.offer(key);
+//             }
+//         }
+//       }
+//       for(boolean room : visited){
+//         if(!room){
+//             return false;
+//         }
+//       }
+//       return true;
+    
+//     }
+// }
+     boolean[] visited = new boolean[rooms.size()];
+     dfs(0,rooms,visited);
+
+     for(boolean room : visited){
         if(!room){
             return false;
         }
-      }
-      return true;
-    
+     }
+     return true;
+    }
+    private void dfs(int node, List<List<Integer>> rooms, boolean[] visited){
+        visited[node] =true;
+
+        for(int neighbour : rooms.get(node)){
+            if(!visited[neighbour]){
+                dfs(neighbour, rooms,visited);
+            }
+        }
     }
 }
