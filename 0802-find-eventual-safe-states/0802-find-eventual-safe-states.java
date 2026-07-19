38dc99bc33d200a47524aff5fe43d58b7d3b9970
Ritesh Suresh Kale <rkale510544@gmail.com>
2026-07-19 23:19:54 +0530

    Time: 4 ms (99.76%), Space: 63.5 MB (32.11%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,42 +1,87 @@
+// BUG in THis 
+
+// class Solution {
+//     public List<Integer> eventualSafeNodes(int[][] graph) {
+//         int n = graph.length;
+
+//         boolean[] visited = new boolean[n];
+//         boolean[] pathVisited = new boolean[n];
+//         boolean[] safe  = new boolean[n];
+
+
+//         for(int i =0; i< n; i++){
+//             if(!visited[i] ){
+//                 dfs(i,graph,visited,pathVisited,safe);
+//             }
+//         }
+//         List<Integer> ans = new ArrayList<>();
+
+//         for(int i =0; i< n; i++){
+//             if(safe[i]){
+//                 ans.add(i);
+//             }
+//         }
+//         return ans;
+//     }
+//     private boolean dfs(int node, int[][] graph, boolean[] visited, boolean[] pathVisited, boolean[] safe){
+//         visited[node] = true;
+//         pathVisited[node] = true;
+
+//         for(int neighbour : graph[node]){
+//             if(!visited[neighbour]){
+//                 if(dfs(neighbour,graph,visited,pathVisited,safe)){
+//                 return true;
+//                 }
+//             } else if(pathVisited[neighbour]){
+//                 return true;
+//             }
+//         }
+//         pathVisited[node] = false;
+//         safe[node] = true;
+
+//         return false;
+//     }
+// }
+
 class Solution {
+
     public List<Integer> eventualSafeNodes(int[][] graph) {
+
         int n = graph.length;
 
-        boolean[] visited = new boolean[n];
-        boolean[] pathVisited = new boolean[n];
-        boolean[] safe  = new boolean[n];
-
-
-        for(int i =0; i< n; i++){
-            if(!visited[i] ){
-                dfs(i,graph,visited,pathVisited,safe);
-            }
-        }
+        int[] state = new int[n];
         List<Integer> ans = new ArrayList<>();
 
-        for(int i =0; i< n; i++){
-            if(safe[i]){
+        for (int i = 0; i < n; i++) {
+
+            if (dfs(i, graph, state)) {
                 ans.add(i);
             }
         }
+
         return ans;
     }
-    private boolean dfs(int node, int[][] graph, boolean[] visited, boolean[] pathVisited, boolean[] safe){
-        visited[node] = true;
-        pathVisited[node] = true;
 
-        for(int neighbour : graph[node]){
-            if(!visited[neighbour]){
-                if(dfs(neighbour,graph,visited,pathVisited,safe)){
-                return true;
-                }
-            } else if(pathVisited[neighbour]){
-                return true;
+    private boolean dfs(int node, int[][] graph, int[] state) {
+
+        // Already processed
+        if (state[node] != 0) {
+            return state[node] == 2;
+        }
+
+        // Visiting
+        state[node] = 1;
+
+        for (int neighbour : graph[node]) {
+
+            if (!dfs(neighbour, graph, state)) {
+                return false;
             }
         }
-        pathVisited[node] = false;
-        safe[node] = true;
 
-        return false;
+        // Safe
+        state[node] = 2;
+
+        return true;
     }
 }
